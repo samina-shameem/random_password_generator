5cc3a37bdf5319cb8bc2b244ee35038fe63b5ba8
samina-shameem <samina.shameem.clt@gmail.com>
2024-01-02 15:57:48 +0100

    checked console log nd crt nw const i/p.

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const btnEl = document.querySelector(".btn")
 
+const inputEl = document.getElementById("input")
+
 btnEl.addEventListener("click",()=>{
     createPassword()
 })
@@ -11,6 +13,8 @@ function createPassword(){
     const passwordLength = 14;
     let password = ""
     for(let index =0; index < passwordLength; index++){
-        const randomNum = Math.random ()* chars.length;
+        const randomNum = Math.floor(Math.random ()* chars.length);
+        password += chars.substring(randomNum, randomNum+1);
+       // console.log(randomNum, password);
     }
 }
