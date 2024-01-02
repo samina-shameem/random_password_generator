6d86c8e48829637174c7464ef24d956164333c11
samina-shameem <samina.shameem.clt@gmail.com>
2024-01-02 15:09:45 +0100

    added nw txt docmt to foldr. crat fun crt pswd nd const , let, use for loop.

--- main.js
+++ main.js
@@ -3,3 +3,14 @@ const btnEl = document.querySelector(".btn")
 btnEl.addEventListener("click",()=>{
     createPassword()
 })
+
+
+function createPassword(){
+    const chars ="0123456789abcdefghijklmnopqrstuvwxtz!@#$%^&()_+?:{}[]ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+    const passwordLength = 14;
+    let password = ""
+    for(let index =0; index < array.length; index++){
+        const element = array[index];
+    }
+}
