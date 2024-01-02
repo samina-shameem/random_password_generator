58d4b38caeb127a45b657a58f21156224516a52a
samina-shameem <samina.shameem.clt@gmail.com>
2024-01-02 16:36:08 +0100

    put dot in clss nd added transition in css.

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const btnEl = document.querySelector(".btn");
 
 const inputEl = document.getElementById("input");
 const copyIconEl =document.querySelector(".fa-copy");
-const alertContainerEl = document.querySelector("alert-container");
+const alertContainerEl = document.querySelector(".alert-container");
 
 btnEl.addEventListener("click",()=>{
     createPassword()
